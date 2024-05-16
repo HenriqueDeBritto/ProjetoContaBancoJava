82452344e14ad47b1f250e516c075599e4a1c863
HenriqueDeBritto <pradohenrique162@gmail.com>
2024-05-16 11:47:34 -0300

    Atualização, emcapsulamento

---
+++ Bancotreino/src/Conta.java
@@ -0,0 +1,42 @@
+public class Conta {
+    private String nome;
+    private String agenciaBanco;
+    private float saque;
+    private int numeroConta;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getAgenciaBanco() {
+        return agenciaBanco;
+    }
+
+    public void setAgenciaBanco(String agenciaBanco) {
+        this.agenciaBanco = agenciaBanco;
+    }
+
+    public float getSaque() {
+        return saque;
+    }
+
+    public void setSaque(float saque) {
+        this.saque = saque;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+    
+
+    
+}
--- Bancotreino/src/ContaTerminal.java
+++ Bancotreino/src/ContaTerminal.java
@@ -2,23 +2,25 @@ import java.util.Scanner;
 
 public class ContaTerminal {
   public static void main(String[] args) throws Exception {
-    java.util.Scanner reader = new Scanner(System.in);
+    var reader = new Scanner(System.in);
+    var conta = new Conta(); 
 
     System.out.print("Olá, introduza seu nome: ");
-    String nome = reader.nextLine();
+    conta.setNome(reader.nextLine());
 
-    System.out.print("Olá ".concat(nome).concat(" introduza o número da sua conta: "));
-    int numeroConta = reader.nextInt();
+    System.out.print("Olá ".concat(conta.getNome()).concat(" introduza o número da sua conta: "));
+    conta.setNumeroConta(reader.nextInt());
     reader.nextLine();
 
     System.out.print("Agora introduza o Número da Agencia: ");
-    String agenciaBanco = reader.nextLine();
+    conta.setAgenciaBanco(reader.nextLine());
 
     System.out.print("Introduza o valor disponivel para saque: ");
-    float saque = reader.nextFloat();
+    conta.setSaque(reader.nextFloat());
 
-    System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "
-        + agenciaBanco + ", conta número " + numeroConta + " e seu saldo " + saque + " já está dísponivel para saque");
+    System.out.println("Olá " + conta.getNome() + ", obrigado por criar uma conta em nosso banco, sua agência é "
+        + conta.getAgenciaBanco() + ", conta número " + conta.getNumeroConta() + " e seu saldo " + conta.getSaque()
+         + " já está dísponivel para saque");
 
   }
 }
